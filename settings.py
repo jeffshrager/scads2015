ff7e9b83873b3e36cb2858b682aa5c57e242163c
Jeff Shrager <jshrager@stanford.edu>
2015-08-20 15:47:15 -0700

    no message

--- settings.py
+++ settings.py
@@ -3,10 +3,10 @@ import driver
 
 global RETRIEVAL_LOW_CC, RETRIEVAL_HIGH_CC, STRATEGY_HIGH_CC, STRATEGY_LOW_CC
 global INCR_on_RIGHT, DECR_on_WRONG, INCR_the_right_answer_on_WRONG
-global epoch, learning_rate, n_problems, strategies, ndups, DR_threshold, experiment_label
+global in_process_training_epochs, learning_rate, n_problems, strategies, ndups, DR_threshold, experiment_label
 global initial_counting_network_burn_in_epochs, initial_counting_network_learning_rate
 global hidden_units, addend_matrix_offby1_delta, PERR, debugging_weight_fill
-global initial_weight_delta, non_result_y_filler
+global non_result_y_filler
 
 hidden_units = 30
 ndups = 1  # Number of replicates of each combo of params -- usually 3 unless testing.
@@ -22,24 +22,27 @@ strategies = [ADD.count_from_either_strategy, ADD.count_from_one_once_strategy,
 # results file because we set these by exec(), this has to have an
 # extra set of "\"quotes\"" around it.
 
-scan_spec = {"settings.experiment_label": ["\"201508201252 Test\""],
+scan_spec = {"settings.experiment_label": ["\"2015081542: Targets centered on 0.0 with -1 range +1\""],
+             # Setting up the initial counting network
+             "settings.initial_counting_network_burn_in_epochs": [1000],
+             "settings.initial_counting_network_learning_rate": [0.1],
+             # Problem presentation and execution
              "settings.n_problems": [5000],
+             "settings.DR_threshold": [1.0],
+             "settings.PERR": [0.0]
+             "settings.addend_matrix_offby1_delta": [1.0], # =1 will make the "next-to" inputs 0, =0 makes them 1, and so on
+             # Choosing to use retrieval v. a strategy
              "settings.RETRIEVAL_LOW_CC": [1.0],
              "settings.RETRIEVAL_HIGH_CC": [1.0],
              "settings.STRATEGY_LOW_CC": [1.0], 
              "settings.STRATEGY_HIGH_CC": [1.0], 
-             "settings.initial_weight_delta": [0.0005],
-             "settings.epoch": [10], # Number of training epochs on EACH test problem
-             "settings.addend_matrix_offby1_delta": [1.0], # =1 will make the "next-to" inputs 0, =0 makes them 1, and so on
-             "settings.non_result_y_filler": [0], # Set into all outputs EXCEPT result, which is adjusted by INCR_RIGHT and DECR_WRONG
-             "settings.INCR_on_RIGHT": [2], # Added to non_result_y_filler at the response value when you get it right.
-             "settings.DECR_on_WRONG": [0], # Substrated from non_result_y_filler at the response value when you get it right.
-             "settings.INCR_the_right_answer_on_WRONG": [2], # Added to non_result_y_filler at the CORRECT value when you get it WRONG.
+             # Learning target params
+             "settings.non_result_y_filler": [0.0], # Set into all outputs EXCEPT result, which is adjusted by INCR_RIGHT and DECR_WRONG
+             "settings.INCR_on_RIGHT": [1.0], # Added to non_result_y_filler at the response value when you get it right.
+             "settings.DECR_on_WRONG": [-1.0], # Substrated from non_result_y_filler at the response value when you get it right.
+             "settings.INCR_the_right_answer_on_WRONG": [0.0], # Added to non_result_y_filler at the CORRECT value when you get it WRONG.
              "settings.learning_rate": [0.001],
-             "settings.initial_counting_network_burn_in_epochs": [1000],
-             "settings.initial_counting_network_learning_rate": [0.1],
-             "settings.DR_threshold": [1.0],
-             "settings.PERR": [0.0]
+             "settings.in_process_training_epochs": [10], # Number of training epochs on EACH test problem
              }
 
 if __name__ == '__main__':
