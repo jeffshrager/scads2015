01427625e307f46fb61888e3fc02105e93f139d3
Myra Cheng <myracheng027@gmail.com>
2016-06-16 15:09:10 -0700

    split into two functions

--- v3/winput.py
+++ v3/winput.py
@@ -1,13 +1,14 @@
 import numpy
-import os
-import numpy
 from random import randint, shuffle, random
 import sys
 
+input = [0] * 5
 def probpres():
-	input = [0] * 5
 	for i in range(0,5):
 		input[i] = [randint(0, 1), randint(0, 1), randint(0, 1), randint(0, 1), randint(0, 1)]
+	return input
+
+def injectNoise():
 	for i in range(0,5):
 		for j in range(0,5):
 			noise = numpy.random.normal(loc=0.0, scale=0.05)
@@ -18,4 +19,6 @@ def probpres():
 			else:
 				input[i][j] -= abs(noise)
 	return input
+
 print probpres()
+print injectNoise()
