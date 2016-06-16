101d50d698d0bdd2f33795ba588930c8ac9b5317
Myra Cheng <myracheng027@gmail.com>
2016-06-16 14:43:18 -0700

    input for W layer

---
+++ v3/winput.py
@@ -0,0 +1,21 @@
+import numpy
+import os
+import numpy
+from random import randint, shuffle, random
+import sys
+
+def probpres():
+	input = [0] * 5
+	for i in range(0,5):
+		input[i] = [randint(0, 1), randint(0, 1), randint(0, 1), randint(0, 1), randint(0, 1)]
+	for i in range(0,5):
+		for j in range(0,5):
+			noise = numpy.random.normal(loc=0.0, scale=0.05)
+			#scale is SD
+			#absolute value of noise? since no negative values
+			if input[i][j] == 0:
+				input[i][j] += abs(noise)
+			else:
+				input[i][j] -= abs(noise)
+	return input
+print probpres()
