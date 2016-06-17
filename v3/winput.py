4c7cb6439e224c919d59fbd92785052e24ecc9db
jeffshrager <jshrager@stanford.edu>
2016-06-17 10:08:10 -0700

    Really used a dictionary (will be useful when we're using words!)

--- v3/winput.py
+++ v3/winput.py
@@ -1,12 +1,11 @@
 ### By Myra; testing input creation for Lingustic model.
 
 import numpy
-import decimal
 from random import randint, shuffle, random
 import sys
-import copy
+import copy # :-) Needed to copy the list.
 
-### These params would end up in the setting class in scads, so 
+### :-) These params would end up in the setting class in scads, so
 ### they're just global here.
 
 n_inputs = 5
@@ -14,10 +13,10 @@ noise_scale = 0.05
 
 class lexical_inputs(object): # :-) Made this a class
 
-      input_dictionary = [0] * n_inputs
+      input_dictionary = {}
 
       def __init__(self):
-	      for i in range(0, n_inputs):
+	      for i in range(1,n_inputs+1):
 		self.input_dictionary[i] = [randint(0, 1) for x in range(n_inputs)] # :-) Used a fancy comprehension here
 
       def noisify(self,v):
@@ -30,19 +29,20 @@ class lexical_inputs(object): # :-) Made this a class
 		      return (v - abs(noise))
 
       def addendWithNoise(self,a): # :-) Rewrote to just get one COPY of the target number with noise added. 
-	      r = copy.copy(self.input_dictionary[1])
+	      r = copy.copy(self.input_dictionary[a])
 	      return [self.noisify(r[x]) for x in range(n_inputs)] # :-) Comprehension again!
       
 		      
 def Rstr(l):
 	return str(['{0:.5f}'.format(v) for v in l])
 
-indict = lexical_inputs() # :-) Init the dictionary array (It's actually not a dictionary!)
 ### Testing
+
+indict = lexical_inputs() # :-) Init the dictionary
+
 for i in range(10):
 	a1 = randint(1,5)
 	a2 = randint(1,5)
 	print(str(a1) + ":" + Rstr(indict.addendWithNoise(a1)) + "+" + str(a2) + ":" + Rstr(indict.addendWithNoise(a2)))
 
-#print init_input_dictionay()
-#print injectNoise()
+
