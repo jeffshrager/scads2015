289636d51636f68bdd127bd5cc29bb3d52d943e8
jeffshrager <jshrager@stanford.edu>
2016-06-17 09:48:04 -0700

    Mod to Myra's test code

--- v3/winput.py
+++ v3/winput.py
@@ -1,24 +1,44 @@
+### By Myra; testing input creation for Lingustic model.
+
 import numpy
 from random import randint, shuffle, random
 import sys
+import copy
 
-input = [0] * 5
-def probpres():
-	for i in range(0,5):
-		input[i] = [randint(0, 1), randint(0, 1), randint(0, 1), randint(0, 1), randint(0, 1)]
-	return input
+### These params would end up in the setting class in scads, so 
+### they're just global here.
 
-def injectNoise():
-	for i in range(0,5):
-		for j in range(0,5):
-			noise = numpy.random.normal(loc=0.0, scale=0.05)
+n_inputs = 5
+noise_scale = 0.05
+
+class lexical_inputs(object): # :-) Made this a class
+
+      input_dictionary = [0] * n_inputs
+
+      def __init__(self):
+	      for i in range(0, n_inputs):
+		self.input_dictionary[i] = [randint(0, 1) for x in range(n_inputs)] # :-) Used a fancy comprehension here
+
+      def noisify(self,v):
+	      noise = numpy.random.normal(loc=0.0, scale=noise_scale)
 			#scale is SD
 			#absolute value of noise? since no negative values
-			if input[i][j] == 0:
-				input[i][j] += abs(noise)
-			else:
-				input[i][j] -= abs(noise)
-	return input
+	      if v == 0:
+		      return (v + abs(noise))
+	      else:
+		      return (v - abs(noise))
 
-print probpres()
-print injectNoise()
+      def addendWithNoise(self,a): # :-) Rewrote to just get one COPY of the target number with noise added. 
+	      r = copy.copy(self.input_dictionary[1])
+	      return [self.noisify(r[x]) for x in range(n_inputs)] # :-) Comprehension again!
+      
+		      
+indict = lexical_inputs() # :-) Init the dictionary array (It's actually not a dictionary!)
+### Testing
+for i in range(10):
+	a1 = randint(1,5)
+	a2 = randint(1,5)
+	print(str(a1) + ":" + str(indict.addendWithNoise(a1)) + "+" + str(a2) + ":" + str(indict.addendWithNoise(a2)))
+
+#print init_input_dictionay()
+#print injectNoise()
