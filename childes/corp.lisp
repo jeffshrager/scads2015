fddd45070ea597b45d29d750d76770b6bbaf8a26
jeffshrager <jshrager@stanford.edu>
2016-07-15 10:54:05 -0700

    copra work

--- childes/corp.lisp
+++ childes/corp.lisp
@@ -28,7 +28,7 @@
   (print file)
   (with-open-file
    (i file)
-   (loop for line = (read-childes-line i nil nil)
+   (loop for line = (read-childes-line i)
 	 until (null line)
 	 do (loop for word in (mapcar #'(lambda (w) (clean-word (clean-word w))) (string-split line :delimiter #\space))
 		  unless (or (zerop (length word)) (member word *stop-words* :test #'string-equal))
@@ -42,7 +42,16 @@
 ;;; file, return nil.
 
 (defun read-childes-line (i)
-  (read-line i nil nil)) ;; OBVIOUSLY THIS DOESN'T WORK YET! FFFWWW
+  ;; For the moment we cheat and only read a line beginning with * and
+  ;; only ONE such line, so if there's several lines, we drop the rest
+  ;; on the floor. FFF WWW XXX
+  (let ((line (read-line i nil nil)))
+    (cond ((null line) nil)
+	  ((char-equal #\* (aref line 0)) line)
+	  (t ""))))
+
+;;; Unfolds deep directories into direct pointers that can go into the
+;;; sources.lisp file
 
 (defun string-split (string &key (delimiter #\,))
   (let ((substrings '())
@@ -75,24 +84,65 @@
 (defparameter *target-words* '("one" "two" "three" "four" "five" "six" "seven" "eight" "nine" "ten" "plus" "count" "many"))
 
 (defun report (&key (targets *target-words*) (min-count 1000))
-  (loop for months being the hash-keys of *age->word->count*
-	using (hash-value word->count)
-	as total-words = (loop for c being the hash-values of word->count sum c)
-	when months
-	do (format t "~%============ Month: ~a Total word count: ~a =============~%" months total-words)
-	(loop for (count . word) in 
-	      (if targets
-		  (loop for word in targets
-			as count = (gethash word word->count)
+  (with-open-file
+   (o "words.xls" :direction :output :if-exists :supersede)
+   (loop for months being the hash-keys of *age->word->count*
+	 using (hash-value word->count)
+	 as total-words = (loop for c being the hash-values of word->count sum c)
+	 when months
+	 do (loop for (count . word) in 
+	       (if targets
+		   (loop for word in targets
+			 as count = (gethash word word->count)
+			 collect (cons count word))
+		 (sort 
+		  (loop for word being the hash-keys of word->count
+			using (hash-value count)
+			when (<= min-count count)
 			collect (cons count word))
-		(sort 
-		    (loop for word being the hash-keys of word->count
-			  using (hash-value count)
-			  when (<= min-count count)
-			  collect (cons count word))
-		    #'> :key #'car))
-	      do (format t "~a	~a	~a~%" word count (when (and word count) (/ (float count) total-words)))
-	      )))
+		  #'> :key #'car))
+	       do (format o "~a	~a	~a	~a	~a~%" 
+			  months total-words word count (when (and word count) (/ (float count) total-words)))
+	       ))))
+
+(defun inverted-report (&key (targets *target-words*) (min-count 1000))
+  (with-open-file
+   (o "wordsinv.xls" :direction :output :if-exists :supersede)
+   (format o "months	")
+   (loop for word in targets do (format o "~a	" word))
+   (format o "~%")
+   (loop for months being the hash-keys of *age->word->count*
+	 using (hash-value word->count)
+	 as total-words = (loop for c being the hash-values of word->count sum c)
+	 when months
+	 do 
+	 (format o "~a	" months)
+	 (loop for word in targets
+	       as count = (gethash word word->count)
+	       do (format o "~a	" (when (and word count) (/ (float count) total-words))))
+	 (format o "~%"))))
+
+(defun get-file-age (file)
+  (with-open-file 
+   (i file)
+   (loop for line = (read-line i nil nil)
+	 as pid = (and line (search "@ID:" line :test #'char-equal))
+	 as chipos = (and line (search "|CHI|" line :test #'char-equal))
+	 do (cond ((null line) (format t "File ~a doesn't have an identifiable age entry!" file) (return nil))
+		  ((and pid (zerop pid) chipos)
+		   (return (subseq line (+ 5 chipos) (position #\| line :start (+ chipos 5)))))))))
+
+(defun xlate-childes-age-rep-to-months (a)
+  ;; The CHILDES says something like: "4;1.09"
+  (let* ((spos (position #\; a))
+	 (dpos (position #\. a))
+	 (y (parse-integer (subseq a 0 spos)))
+	 (m (parse-integer (subseq a (1+ spos) dpos))))
+    (+ (* 12 y) m)))
+
+(defun sourceify (file)
+  (let ((a (get-file-age file)))
+    (when a (print `(,(xlate-childes-age-rep-to-months a) ,file)))))
 
 (defun test (&key reset)
   (when (or reset (zerop (hash-table-count *age->word->count*)))
@@ -100,7 +150,8 @@
     (clrhash *age->word->count*)
     (scrape-sources)
     )
-  (report) ;; reports only *target-words*
+  (inverted-report)
+  ;(report) ;; reports only *target-words*
   ;(report :targets nil) ;; Reports all words
   )
 
@@ -108,4 +159,3 @@
 ;(trace fnnx)
 ;(test :reset t)
 (test)
-
